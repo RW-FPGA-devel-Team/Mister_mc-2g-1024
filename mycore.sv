9ce7929a7178cc53394f26e86d6f859533654d6b
Ramón Martínez <rampa@encomix.org>
2020-09-07 23:51:53 +0200

    initial release

--- mycore.sv
+++ mycore.sv
@@ -1,4 +1,14 @@
 //============================================================================
+//  Grant’s multi computer
+// 
+//  Port to MiSTer.
+//
+//  Based on Grant’s multi computer
+//  http://searle.hostei.com/grant/
+//  http://searle.hostei.com/grant/Multicomp/index.html
+//	 and WiSo's collector blog (MiST port)
+//	 https://ws0.org/building-your-own-custom-computer-with-the-mist-fpga-board-part-1/
+//	 https://ws0.org/building-your-own-custom-computer-with-the-mist-fpga-board-part-2/
 //
 //  This program is free software; you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License as published by the Free
@@ -13,8 +23,8 @@
 //  You should have received a copy of the GNU General Public License along
 //  with this program; if not, write to the Free Software Foundation, Inc.,
 //  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
 //============================================================================
+
 
 module emu
 (
@@ -48,38 +58,9 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 
-	/*
-	// Use framebuffer from DDRAM (USE_FB=1 in qsf)
-	// FB_FORMAT:
-	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
-	//    [3]   : 0=16bits 565 1=16bits 1555
-	//    [4]   : 0=RGB  1=BGR (for 16/24/32 modes)
-	//
-	// FB_STRIDE either 0 (rounded to 256 bytes) or multiple of 16 bytes.
-	output        FB_EN,
-	output  [4:0] FB_FORMAT,
-	output [11:0] FB_WIDTH,
-	output [11:0] FB_HEIGHT,
-	output [31:0] FB_BASE,
-	output [13:0] FB_STRIDE,
-	input         FB_VBL,
-	input         FB_LL,
-	output        FB_FORCE_BLANK,
-
-	// Palette control for 8bit modes.
-	// Ignored for other video modes.
-	output        FB_PAL_CLK,
-	output  [7:0] FB_PAL_ADDR,
-	output [23:0] FB_PAL_DOUT,
-	input  [23:0] FB_PAL_DIN,
-	output        FB_PAL_WR,
-	*/
+	
 
 	output        LED_USER,  // 1 - ON, 0 - OFF.
-
-	// b[1]: 0 - LED status is system status OR'd with b[0]
-	//       1 - LED status is controled solely by b[0]
-	// hint: supply 2'b00 to let the system control the LED.
 	output  [1:0] LED_POWER,
 	output  [1:0] LED_DISK,
 
@@ -137,23 +118,18 @@ module emu
 	output        UART_DTR,
 	input         UART_DSR,
 
-	// Open-drain User port.
-	// 0 - D+/RX
-	// 1 - D-/TX
-	// 2..6 - USR2..USR6
-	// Set USER_OUT to 1 to read from USER_IN.
+
 	input   [6:0] USER_IN,
 	output  [6:0] USER_OUT,
 
 	input         OSD_STATUS
 );
 
-///////// Default values for ports not used in this core /////////
 
 assign ADC_BUS  = 'Z;
 assign USER_OUT = '1;
-assign {UART_RTS, UART_TXD, UART_DTR} = 0;
-assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
+//assign {UART_RTS, UART_TXD, UART_DTR} = 0;
+//assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
 assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = '0;  
 
@@ -165,120 +141,179 @@ assign AUDIO_L = 0;
 assign AUDIO_R = 0;
 assign AUDIO_MIX = 0;
 
-assign LED_DISK = 0;
-assign LED_POWER = 0;
 assign BUTTONS = 0;
 
-//////////////////////////////////////////////////////////////////
+assign LED_USER  = 0;
+assign LED_DISK  = ~driveLED;
+assign LED_POWER = 0;
 
-assign VIDEO_ARX = status[1] ? 8'd16 : 8'd4;
-assign VIDEO_ARY = status[1] ? 8'd9  : 8'd3; 
+assign VIDEO_ARX = 4;
+assign VIDEO_ARY = 3;
 
-`include "build_id.v" 
+
+`include "build_id.v"
 localparam CONF_STR = {
-	"MyCore;;",
-	"-;",
-	"O1,Aspect ratio,4:3,16:9;",
-	"O2,TV Mode,NTSC,PAL;",
-	"O34,Noise,White,Red,Green,Blue;",
-	"-;",
-	"P1,Test Page 1;",
-	"P1-;",
-	"P1-, -= Options in page 1 =-;",
-	"P1-;",
-	"P1O5,Option 1-1,Off,On;",
-	"d0P1F1,BIN;",
-	"H0P1O6,Option 1-2,Off,On;",
-	"-;",
-	"P2,Test Page 2;",
-	"P2-;",
-	"P2-, -= Options in page 2 =-;",
-	"P2-;",
-	"P2S0,DSK;",
-	"P2O67,Option 2,1,2,3,4;",
-	"-;",
+	"MultiComp;;",
 	"-;",
 	"T0,Reset;",
-	"R0,Reset and close OSD;",
-	"V,v",`BUILD_DATE 
+	"-;",
+	"V,v1.1.",`BUILD_DATE
 };
 
-wire forced_scandoubler;
+
+//////////////////   HPS I/O   ///////////////////
 wire  [1:0] buttons;
 wire [31:0] status;
-wire [10:0] ps2_key;
 
-hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
+wire PS2_CLK;
+wire PS2_DAT;
+
+wire forced_scandoubler;
+
+wire [31:0] sd_lba;
+wire        sd_rd;
+wire        sd_wr;
+wire        sd_ack;
+wire  [8:0] sd_buff_addr;
+wire  [7:0] sd_buff_dout;
+wire  [7:0] sd_buff_din;
+wire        sd_buff_wr;
+wire        sd_ack_conf;
+
+
+wire pll_locked;
+wire clk_sys;
+
+
+pll pll
+(
+  .refclk  (CLK_50M),
+  .rst     (0),
+  .outclk_0(clk_sys),
+  .locked  (pll_locked)
+);
+
+
+
+hps_io #(.STRLEN($size(CONF_STR)>>3), .PS2DIV(1923)) hps_io
 (
 	.clk_sys(clk_sys),
 	.HPS_BUS(HPS_BUS),
-	.EXT_BUS(),
-	.gamma_bus(),
 
 	.conf_str(CONF_STR),
-	.forced_scandoubler(forced_scandoubler),
 
 	.buttons(buttons),
 	.status(status),
-	.status_menumask({status[5]}),
-	
-	.ps2_key(ps2_key)
+	.forced_scandoubler(forced_scandoubler),
+
+	.ps2_kbd_clk_out(PS2_CLK),
+	.ps2_kbd_data_out(PS2_DAT)
 );
 
-
-///////////////////////   CLOCKS   ///////////////////////////////
-
-wire clk_sys;
-pll pll
-(
-	.refclk(CLK_50M),
-	.rst(0),
-	.outclk_0(clk_sys)
-);
+/////////////////  RESET  /////////////////////////
 
 wire reset = RESET | status[0] | buttons[1];
 
-//////////////////////////////////////////////////////////////////
-
-wire [1:0] col = status[4:3];
-
-wire HBlank;
-wire HSync;
-wire VBlank;
-wire VSync;
-wire ce_pix;
-wire [7:0] video;
-
-mycore mycore
-(
-	.clk(clk_sys),
-	.reset(reset),
-	
-	.pal(status[2]),
-	.scandouble(forced_scandoubler),
-
-	.ce_pix(ce_pix),
-
-	.HBlank(HBlank),
-	.HSync(HSync),
-	.VBlank(VBlank),
-	.VSync(VSync),
-
-	.video(video)
-);
+///////////////////////////////////////////////////
 
 assign CLK_VIDEO = clk_sys;
-assign CE_PIXEL = ce_pix;
 
-assign VGA_DE = ~(HBlank | VBlank);
-assign VGA_HS = HSync;
-assign VGA_VS = VSync;
-assign VGA_G  = (!col || col == 2) ? video : 8'd0;
-assign VGA_R  = (!col || col == 1) ? video : 8'd0;
-assign VGA_B  = (!col || col == 3) ? video : 8'd0;
 
-reg  [26:0] act_cnt;
-always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
-assign LED_USER    = act_cnt[26]  ? act_cnt[25:18]  > act_cnt[7:0]  : act_cnt[25:18]  <= act_cnt[7:0];
+
+wire hblank, vblank;
+wire hs, vs;
+wire [1:0] r,g,b;
+wire driveLED;
+
+wire [2:0] _hblank, _vblank;
+wire [2:0] _hs, _vs;
+wire [1:0] _r;
+wire [1:0] _g;
+wire [1:0] _b;
+wire [2:0] _CE_PIXEL;
+wire [2:0] _SD_CS;
+wire [2:0] _SD_MOSI;
+wire [2:0] _SD_SCK;
+wire [2:0] _driveLED;
+
+always_comb 
+begin
+	hblank 		<= _hblank;
+	vblank 		<= _vblank;
+	hs 		 	<= _hs;
+	vs				<= _vs;
+	r 				<= _r[1:0];
+	g 				<= _g[1:0];
+	b				<= _b[1:0];
+	CE_PIXEL		<= _CE_PIXEL;
+	SD_CS			<= _SD_CS;
+	SD_MOSI		<= _SD_MOSI;
+	SD_SCK		<= _SD_SCK;
+	driveLED 	<= _driveLED;
+end
+
+Microcomputer Microcomputer
+(
+	.n_reset(~reset),
+	.clk(clk_sys),
+	//
+  	.sramData		(),
+	.sramAddress	(),
+	.n_sRamWE		(),
+	.n_sRamOE		(),
+	.n_sRam1CS		(),
+	.n_sRam2CS		(),
+
+	//
+	.videoR0 (_r[0]),
+	.videoR1 (_r[1]),
+	.videoG0 (_g[0]),
+	.videoG1 (_g[1]),
+	.videoB0 (_b[0]),
+	.videoB1 (_b[1]),
+	.hSync   (_hs[0]),
+	.vSync   (_vs[0]),
+	.hBlank(_hblank[0]),
+	.vBlank(_vblank[0]),
+	.cepix(_CE_PIXEL[0]),
+	//
+	.ps2Clk(PS2_CLK),
+	.ps2Data(PS2_DAT),
+	//
+	.rxd1		(UART_RXD),
+	.txd1		(UART_TXD),
+	.rts1		(UART_RTS),
+	.cts1		(UART_CTS),
+
+	.sdCS(_SD_CS[0]),
+	.sdMOSI(_SD_MOSI[0]),
+	.sdMISO(SD_MISO),
+	.sdSCLK(_SD_SCK[0]),
+	.driveLED(_driveLED[0])
+);
+		
+
+
+video_cleaner video_cleaner
+(
+	.clk_vid(CLK_VIDEO),
+	.ce_pix(CE_PIXEL),
+
+	.R({4{r}}),
+	.G({4{g}}),
+	.B({4{b}}),
+	.HSync(hs),
+	.VSync(vs),
+	.HBlank(hblank),
+	.VBlank(vblank),
+
+	.VGA_R(VGA_R),
+	.VGA_G(VGA_G),
+	.VGA_B(VGA_B),
+	.VGA_VS(VGA_VS),
+	.VGA_HS(VGA_HS),
+	.VGA_DE(VGA_DE)
+);
+
 
 endmodule
--- rtl/pll.v
+++ rtl/pll.v
@@ -50,7 +50,7 @@ endmodule
 // Retrieval info: 	<generic name="debug_print_output" value="false" />
 // Retrieval info: 	<generic name="debug_use_rbc_taf_method" value="false" />
 // Retrieval info: 	<generic name="device_family" value="Cyclone V" />
-// Retrieval info: 	<generic name="device" value="5CEBA2F17A7" />
+// Retrieval info: 	<generic name="device" value="Unknown" />
 // Retrieval info: 	<generic name="gui_device_speed_grade" value="2" />
 // Retrieval info: 	<generic name="gui_pll_mode" value="Integer-N PLL" />
 // Retrieval info: 	<generic name="gui_reference_clock_frequency" value="50.0" />
@@ -66,7 +66,7 @@ endmodule
 // Retrieval info: 	<generic name="gui_frac_multiply_factor" value="1" />
 // Retrieval info: 	<generic name="gui_divide_factor_n" value="1" />
 // Retrieval info: 	<generic name="gui_cascade_counter0" value="false" />
-// Retrieval info: 	<generic name="gui_output_clock_frequency0" value="20.0" />
+// Retrieval info: 	<generic name="gui_output_clock_frequency0" value="50.0" />
 // Retrieval info: 	<generic name="gui_divide_factor_c0" value="1" />
 // Retrieval info: 	<generic name="gui_actual_output_clock_frequency0" value="0 MHz" />
 // Retrieval info: 	<generic name="gui_ps_units0" value="ps" />
